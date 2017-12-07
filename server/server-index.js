648eba391039f3e2fece7b5b26a5228147d0a504
dwrz <dwrz@dwrz.net>
2017-12-07 20:28:03 +0000

    Reformat whitespace

--- server/server-index.js
+++ server/server-index.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = 3000;
 
 app.use(express.static(`${__dirname}/../client/dist/`));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.listen(PORT, () => {
