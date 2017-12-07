c7a778d71ad63dd2cc6abc6e5c639364d908f0b9
dwrz <dwrz@dwrz.net>
2017-12-07 22:22:24 +0000

    Add test for server response at root

--- tests/server-test.js
+++ tests/server-test.js
@@ -1,1 +1,12 @@
+const chai = require('chai');
 const { expect } = require('chai');
+const chaiHTTP = require('chai-http');
+
+chai.use(chaiHTTP);
+
+chai.request('http://localhost:3000')
+  .get('/')
+  .end((err, res) => {
+    expect(err).to.be.null;
+    expect(res).to.have.status(200);
+  });
