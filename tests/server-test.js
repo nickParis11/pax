13bc4b1216c5276c5df0ac5f0463a26a9616c382
dwrz <dwrz@dwrz.net>
2017-12-08 18:57:14 +0000

    Add tests for server routes

--- tests/server-test.js
+++ tests/server-test.js
@@ -10,3 +10,31 @@ chai.request('http://localhost:3000')
     expect(err).to.be.null;
     expect(res).to.have.status(200);
   });
+
+chai.request('http://localhost:3000')
+  .post('/api/analyze')
+  .end((err, res) => {
+    expect(err).to.be.null;
+    expect(res).to.have.status(200);
+  });
+
+chai.request('http://localhost:3000')
+  .get('/api/vote')
+  .end((err, res) => {
+    expect(err).to.be.null;
+    expect(res).to.have.status(200);
+  });
+
+chai.request('http://localhost:3000')
+  .post('/login')
+  .end((err, res) => {
+    expect(err).to.be.null;
+    expect(res).to.have.status(200);
+  });
+
+chai.request('http://localhost:3000')
+  .post('/signup')
+  .end((err, res) => {
+    expect(err).to.be.null;
+    expect(res).to.have.status(200);
+  });
