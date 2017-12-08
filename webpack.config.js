fc08d6f33c7da45ddc7c3223238d40ceeea13ebd
dwrz <dwrz@dwrz.net>
2017-12-08 01:36:12 +0000

    Reformat webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const path = require('path');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const CompressionPlugin = require('compression-webpack-plugin');
 
-
 const BUILD_DIR = path.resolve(__dirname, 'client/dist');
 const APP_DIR = path.resolve(__dirname, 'client/src');
 
@@ -23,31 +22,30 @@ const config = {
       },
       {
         test: /\.(png|jpg)$/,
-        		loader: 'url-loader?limit=8192',
+        loader: 'url-loader?limit=8192',
       },
     ],
   },
-
   plugins: [
-	   new UglifyJsPlugin({
-	    sourceMap: false,
-	  }),
-	   new webpack.DefinePlugin({
-	    'process.env': {
-	      NODE_ENV: JSON.stringify('production'),
-	    },
-	  }),
-	  new webpack.optimize.DedupePlugin(), /* ,
-	  new CompressionPlugin({
-	        asset: "[path].gz[query]",
-	        algorithm: "gzip",
-	        test: /\.js$|\.css$|\.html$/,
-	        threshold: 10240,
-	        minRatio: 0.8
-	      })
-	   */
+    new UglifyJsPlugin({
+      sourceMap: false,
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'),
+      },
+    }),
+    new webpack.optimize.DedupePlugin(),
+    /* ,
+       new CompressionPlugin({
+       asset: "[path].gz[query]",
+       algorithm: "gzip",
+       test: /\.js$|\.css$|\.html$/,
+       threshold: 10240,
+       minRatio: 0.8
+       })
+    */
   ],
-
 };
 
 module.exports = config;
