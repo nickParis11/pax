5404ba5ca26d7613a72a29e69096a13929d58d7e
dwrz <dwrz@dwrz.net>
2017-12-08 18:57:36 +0000

    Add server routes

--- server/server-index.js
+++ server/server-index.js
@@ -8,6 +8,22 @@ app.use(express.static(`${__dirname}/../client/dist/`));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.post('/api/analyze', (req, res) => {
+  res.send(null);
+});
+
+app.post('/api/vote', (req, res) => {
+  res.send(null);
+});
+
+app.post('/login', (req, res) => {
+  res.send(null);
+});
+
+app.post('/signup', (req, res) => {
+  res.send(null);
+});
+
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}`);
 });
