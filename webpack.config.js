a2f3205addfd7e78a19ee086660c4e48ab0f0017
Niklas <norr1000@gmail.com>
2017-12-08 01:02:12 +0100

    add webpack original config

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const webpack = require('webpack');
+const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
+
+
+const BUILD_DIR = path.resolve(__dirname, 'client/dist');
+const APP_DIR = path.resolve(__dirname, 'client/src');
+
+const config = {
+  devtool: 'souce-map',
+  entry: `${APP_DIR}/Index.jsx`,
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?/,
+        include: APP_DIR,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.(png|jpg)$/,
+        		loader: 'url-loader?limit=8192',
+      },
+    ],
+  },
+
+  plugins: [
+	   new UglifyJsPlugin({
+	    sourceMap: false,
+	  }),
+	   new webpack.DefinePlugin({
+	    'process.env': {
+	      NODE_ENV: JSON.stringify('production'),
+	    },
+	  }),
+	  new webpack.optimize.DedupePlugin(), /* ,
+	  new CompressionPlugin({
+	        asset: "[path].gz[query]",
+	        algorithm: "gzip",
+	        test: /\.js$|\.css$|\.html$/,
+	        threshold: 10240,
+	        minRatio: 0.8
+	      })
+	   */
+  ],
+
+};
