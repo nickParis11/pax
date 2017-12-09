4f26550a307d33cf6ea21909f9dcafc60bef6304
dwrz <dwrz@dwrz.net>
2017-12-09 00:42:56 +0000

    Fix syntax in update-production script

--- scripts/update-production.sh
+++ scripts/update-production.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 git pull --rebase upstream master
-if [$? -eq 0]; then
+if  [ $? -eq 0 ]; then
     npm install
-    if [$? -eq 0]; then
+    if [ $? -eq 0 ]; then
         npm run build
-        if [$? eq 0]; then
+        if [ $? eq 0 ]; then
             pm2 restart server-index
             printf 'Update completed succesfully.'
             exit 0
