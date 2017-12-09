0d6b484818b9614a20a5f72eecf048b46e3e1026
Katelyn Stamas <cakelyn@gmail.com>
2017-12-09 13:25:21 -0500

    remove pseudocode

--- client/src/store.js
+++ client/src/store.js
@@ -2,19 +2,7 @@ import { applyMiddleware, createStore } from 'redux';
 import { createLogger } from 'redux-logger';
 import thunk from 'redux-thunk';
 import reducer from './reducers';
-// import axios from 'axios';
 
 const logger = createLogger();
 const middleware = applyMiddleware(thunk, logger);
 export default createStore(reducer, middleware);
-
-// store.dispatch((dispatch) => {
-//   dispatch({ type: 'FETCH_DATA_START' });
-//   axios.get('https://jsonplaceholder.typicode.com/users')
-//     .then((response) => {
-//       dispatch({ type: 'RECEIVE_DATA', payload: response.data });
-//     })
-//     .catch((err) => {
-//       dispatch({ type: 'FETCH_DATA_ERROR', payload: err });
-//     });
-// });
