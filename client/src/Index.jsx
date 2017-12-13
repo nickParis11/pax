bf84836c05bdfe668fd66b18a043c1f44d4f8de4
Katelyn Stamas <cakelyn@gmail.com>
2017-12-13 14:39:25 -0500

    Install react router

--- client/src/Index.jsx
+++ client/src/Index.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import Analyzer from './components/Analyzer.jsx';
 import { Provider } from 'react-redux';
 import store from './store.js';
 
 ReactDOM.render(<Provider store={store}>
-  <Analyzer />
+  <BrowserRouter>
+    <Analyzer />
+  </BrowserRouter>
 </Provider>, document.getElementById('app'));
