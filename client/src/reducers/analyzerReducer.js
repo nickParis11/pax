7e12a270aa24a64f40fdefdbf26f2140f9afa1e9
Katelyn Stamas <cakelyn@gmail.com>
2017-12-13 18:16:29 -0500

    Refactor userReducers to switch statement

--- client/src/reducers/analyzerReducer.js
+++ client/src/reducers/analyzerReducer.js
@@ -10,23 +10,32 @@ const initialState = {
 };
 
 export default function analyzer(state = initialState, action) {
-  if (action.type === 'TOGGLE_URL_TRUE') {
-    return { ...state, analyzeUrl: false, analyzeText: true };
-  } else if (action.type === 'TOGGLE_TEXT_TRUE') {
-    return { ...state, analyzeUrl: true, analyzeText: false };
-  } else if (action.type === 'ANALYZE_INPUT') {
-    return { ...state, input: action.payload };
-  } else if (action.type === 'ANALYSIS_SUBMITTED') {
-    return { ...state, success: false, waiting: true };
-  } else if (action.type === 'TONE_RESULTS_REJECTED') {
-    return { ...state, error: action.payload };
-  } else if (action.type === 'SENTIMENT_RESULTS_REJECTED') {
-    return { ...state, error: action.payload };
-  } else if (action.type === 'RESULTS_FULFILLED') {
-    return { ...state, tone: action.tone, sentiment: action.sentiment };
-  } else if (action.type === 'ANALYSIS_FULFILLED') {
-    return { ...state, success: true, waiting: false };
+  switch(action.type) {
+    case 'TOGGLE_URL_TRUE':
+      return { ...state, analyzeUrl: false, analyzeText: true };
+      break;
+    case 'TOGGLE_TEXT_TRUE':
+      return { ...state, analyzeUrl: true, analyzeText: false };
+      break;
+    case 'ANALYZE_INPUT':
+      return { ...state, input: action.payload };
+      break;
+    case 'ANALYSIS_SUBMITTED':
+      return { ...state, success: false, waiting: true };
+      break;
+    case 'TONE_RESULTS_REJECTED':
+      return { ...state, error: action.payload };
+      break;
+    case 'SENTIMENT_RESULTS_REJECTED':
+      return { ...state, error: action.payload };
+      break;
+    case 'RESULTS_FULFILLED':
+      return { ...state, tone: action.tone, sentiment: action.sentiment };
+      break;
+    case 'ANALYSIS_FULFILLED':
+      return { ...state, success: true, waiting: false };
+      break;
+    default:
+      return state;
   }
-
-  return state;
 }
