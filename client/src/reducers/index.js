776190963bbc365ee8c37ef33b79dc7474359c65
dwrz <dwrz@dwrz.net>
2017-12-16 02:53:56 +0000

    Add voteReducer to combineReducers

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import analyzer from './analyzerReducer';
 import user from './userReducer';
+import vote from './voteReducer.js';
 
 export default combineReducers({
   analyzer,
   user,
+  vote,
 });
