721f2ddbc493d102b1fa4722cc9b90fd0c7749d6
dwrz <dwrz@dwrz.net>
2017-12-16 02:55:12 +0000

    Add voteActions

---
+++ client/src/actions/voteActions.js
@@ -0,0 +1,46 @@
+import axios from 'axios';
+
+export function downvote() {
+  return (dispatch) => {
+    dispatch({ type: 'DOWNVOTE' });
+    axios.post('/api/upvote') // Will need to specify some kind of identifier for the article.
+      .then((res) => { // Expects updated downvote number.
+        dispatch({ type: 'DOWNVOTE_LOGGED', payload: res.data });
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+}
+
+// Get user vote status (up/down/nil) and total vote count (up and down).
+export function getArticleVoteData() {
+  return (dispatch) => {
+    axios.get('/api/votes') // Will need to specify some kind of identifier for the article.
+      .then((res) => { // Expects updated downvote number.
+        dispatch({
+          type: 'UPDATE_VOTE_DATA',
+          payload: {
+            downVoteCount: res.data.downVoteCount,
+            upVoteCount: res.data.upVoteCount,
+          },
+        });
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+}
+
+export function upvote() {
+  return (dispatch) => {
+    dispatch({ type: 'UPVOTE' });
+    axios.post('/api/upvote') // Will need to specify some kind of identifier for the article.
+      .then((res) => {
+        dispatch({ type: 'UPVOTE_LOGGED', payload: res.data });
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+}
