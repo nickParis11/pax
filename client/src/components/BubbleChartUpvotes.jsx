9e3c183e90f9270cb29b1da04c3ff3f84e25541e
dwrz <dwrz@dwrz.net>
2017-12-17 01:06:28 +0000

    Reformat to align to style guide

--- client/src/components/BubbleChartUpvotes.jsx
+++ client/src/components/BubbleChartUpvotes.jsx
@@ -1,24 +1,27 @@
 import React from 'react';
 import ReactFauxDOM from 'react-faux-dom';
-import * as d3 from 'd3';
 import { connect } from 'react-redux';
+// import * as d3 from 'd3';
 
-@connect(store => {
-  height: store.analyzer.height,
-  sentiment: store.analyzer.sentiment,
-  tone: store.analyzer.tone,
-  width: store.analyzer.width,
-})
-
-export default class BubbleChartUpvotes extends React.Component {
+@connect((store) => {
+  return {
+    height: store.analyzer.height,
+    sentiment: store.analyzer.sentiment,
+    tone: store.analyzer.tone,
+    width: store.analyzer.width,
+  };
+}) export default class BubbleChartUpvotes extends React.Component {
   drawChart() {
     const div = new ReactFauxDOM.Element('div');
-
-    return div.toReact()
+    return div.toReact();
   }
 
-  render () {
-    return this.drawChart();
+  render() {
+    return (
+      <div className="container">
+        {this.drawChart()}
+      </div>
+    );
   }
 }
-articles I tend to agree = articles I upvote
+// articles I tend to agree = articles I upvote
