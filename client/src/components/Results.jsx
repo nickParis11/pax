f9f51555d9ea6d17b18aa95ebc97fef446f5c5ac
Katelyn Stamas <cakelyn@gmail.com>
2017-12-17 13:21:46 -0500

    Move vote arrows to left and align

--- client/src/components/Results.jsx
+++ client/src/components/Results.jsx
@@ -27,35 +27,47 @@ import { downvote, getArticleVoteData, upvote } from '../actions/voteActions.js'
   }
 
   render() {
+    const tone = this.props.tone.document_tone;
+    console.log(this.props);
+    const sentiment = this.props.sentiment;
+
     return (
       <div>
         <h1 className="componentTitle">RESULTS</h1>
-        <h2 className="trustRating">Trust Rating: {this.props.score}</h2>
+        <h2 className="trustRating">Trust Rating: {this.props.score}%</h2>
         <div className="row">
+          <div className="arrows">
+            <div className="arrowsContainer">
+              <span className={this.props.upVote ? 'arrow arrowUpSelected' : 'arrow'} onClick={this.voteUp.bind(this)}>↑</span>
+              <Badge
+                badgeContent={this.props.upVoteCount}
+                primary={true}
+                >
+              </Badge>
+            </div>
+            <div className="arrowsContainer">
+              <span className={this.props.downVote ? 'arrow arrowDownSelected' : 'arrow'} onClick={this.voteDown.bind(this)}>↓</span>
+              <Badge
+                badgeContent={this.props.downVoteCount}
+                secondary={true}
+                >
+              </Badge>
+            </div>
+          </div>
+          <div></div>
           <div className="summaryContainer">
             <p><b>Summary: </b>
               <br />
-              Polarity: {this.props.sentiment.polarity}
+              Polarity: {sentiment.polarity}
               <br />
               Health goth DIY ramps skateboard.
               <br />
               IPhone 8-bit williamsburg cronut try-hard humblebrag.
             </p>
           </div>
-          <div className="arrowsContainer">
-            <span className={this.props.upVote ? 'arrow arrowUpSelected' : 'arrow'} onClick={this.voteUp.bind(this)}>↑</span>
-            <Badge
-              badgeContent={this.props.upVoteCount}
-              primary
-            />
-            <span className={this.props.downVote ? 'arrow arrowDownSelected' : 'arrow'} onClick={this.voteDown.bind(this)}>↓</span>
-            <Badge
-              badgeContent={this.props.downVoteCount}
-              secondary
-            />
-          </div>
         </div>
-      </div>
+        <div></div>
+        </div>
     );
   }
 }
