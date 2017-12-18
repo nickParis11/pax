b9bbf76be4034e98dba38684aa5023c99c081b95
Alexander Mire <amire51117@gmail.com>
2017-12-18 14:50:45 -0500

    hovering over now shows tone and score

--- client/src/components/EmotionChart.jsx
+++ client/src/components/EmotionChart.jsx
@@ -13,8 +13,6 @@ import { connect } from 'react-redux';
   drawChart() {
     const div = new ReactFauxDOM.Element('div');
     const tone = this.props.tone.document_tone;
-    // const sentiment = this.props.sentiment;
-    // emotion list array in ['emotion type', score] format
     const emotionList = tone.tone_categories[0].tones.map(emo =>
       [emo.tone_name, Math.round(emo.score * 100)]);
     const languageToneList = tone.tone_categories[1].tones.map(lang =>
@@ -35,35 +33,24 @@ import { connect } from 'react-redux';
       .rangeRound([0, width])
       .paddingInner(10)
       .paddingOuter(25);
+
     const y = d3
       .scaleLinear()
       .range([height, 0]);
 
-    // const valueline = d3.line()
-    //   .data(data)
-    //   .x( d => {return x(d[0])})
-    //   .y( d => {return y(d[1])});
-
-    // const xAxis = d3
-    //   .axisBottom()
-    //   .scale(x);
-
-    // const yAxis = d3
-    //   .axisLeft()
-    //   .scale(y)
-    //   .tickFormat(d3.format('10'));
     const svg = d3
       .select(div)
       .append('svg')
       .attr('width', width + margin.left + margin.right)
       .attr('height', height + margin.top + margin.bottom)
-      //     .style('background-color', '#E5DF24')
       .append('g')
       .attr('transform', `translate(${margin.left},${margin.top})`);
-    x.domain(data.map(d => d[0]));
-    y.domain([0, /* d3.max(data, (d)=> d[1]) */100]);
 
-    // create axis
+    //create range
+    x.domain(data.map(d => d[0]));
+    y.domain([0, 100]);
+
+    // create x-axis
     svg.append('g')
       .attr('class', 'x axis')
       .attr('transform', `translate(0, ${-height}`)
@@ -74,19 +61,18 @@ import { connect } from 'react-redux';
       .attr('dy', '.15em')
       .attr('transform', 'rotate(-65)');
 
-    // add text label
+    // x-axis text label
     svg.append('text')
-      // .attr('transform', `translate(${width/2} ,${height + margin.top + 30}`)
       .style('text-anchor', 'middle')
-      // .style('text-anchor', 'end')
       .attr('dx', '22em')
       .attr('dy', '-.5em')
       .text('Tone Type');
 
+    //create y-axis
     svg.append('g')
-      // .attr('class', 'y axis')
       .call(d3.axisLeft(y));
 
+    // y-axis text label
     svg.append('text')
       .attr('transform', 'rotate(-90)')
       .attr('y', 0)
@@ -95,6 +81,7 @@ import { connect } from 'react-redux';
       .style('text-anchor', 'end')
       .text('Tone level (%)');
 
+    //values for both axis
     svg.selectAll('.bar')
       .data(data)
       .enter().append('rect')
@@ -102,51 +89,9 @@ import { connect } from 'react-redux';
       .attr('x', d => x(d[0]))
       .attr('width', 20)
       .attr('y', d => y(d[1]))
-    //      .style('fill', '#800080')
-      .attr('height', d => height - y(d[1]));
-
-    // var selector = d3.select('#drop')
-    // .append('select')
-    // .attr('id','dropdown')
-    // .on('change', function(d){
-    //   selection = document.getElementById('dropdown');
-    //   y.domain([0,d3.max(data, function(d) {
-    //     return +d[1]
-    //   })]);
-    //   yAxis.scale(y);
-
-    //   d3.selectAll('.bar')
-    //     .transition()
-    //     .attr('height', (d) => {
-    //       return height - y(+d[1]);
-    //     })
-    //     .attr('x', (d, i )=> {
-    //       return (width / data.length) * i
-    //     })
-    //     .attr('y', (d)=>{
-    //       return y(+d[1]);
-    //     })
-    //     .ease('linear')
-    //     .select('title')
-    //     .text((d)=>{
-    //       return d[0] + ':' + d[1];
-    //     });
-
-    //   d3.selectAll('g.y.axis')
-    //     .transition()
-    //     .call(yAxis);
-
-    //   selector.selectAll("option")
-    //   .data(elements)
-    //   .enter().append("option")
-    //   .attr("value", function(d){
-    //     return d;
-    //   })
-    //   .text(function(d){
-    //     return d;
-    //   })
-    // })
-
+      .attr('height', d => height - y(d[1]))
+      .append("svg:title")
+      .text(function(d) { return d[0] + ':' + d[1]; });
 
     return div.toReact();
   }
