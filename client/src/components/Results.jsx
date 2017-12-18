3f2b52e8e2bbe1d0df6e0fa64e75ac9f186d9168
dwrz <dwrz@dwrz.net>
2017-12-18 19:20:46 +0000

    Refactor Results to use ResultsVote subcomponent

--- client/src/components/Results.jsx
+++ client/src/components/Results.jsx
@@ -1,39 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import Badge from 'material-ui/Badge';
 import EmotionChart from './EmotionChart.jsx';
-import { downvote, getArticleVoteData, upvote } from '../actions/voteActions.js';
+import ResultsVote from './ResultsVote.jsx';
 
 @connect((store) => {
   return {
-    downVote: store.vote.downVote,
-    downVoteCount: store.vote.downVoteCount,
     score: store.analyzer.score,
     sentiment: store.analyzer.sentiment,
     success: store.analyzer.success,
-    upVote: store.vote.upVote,
-    upVoteCount: store.vote.upVoteCount,
   };
 }) export default class Results extends React.Component {
-  componentDidMount() {
-    this.props.dispatch(getArticleVoteData());
-  }
-
-  handleKeyPress(e) {
-    if (e.key === 'ArrowDown') {
-      this.props.dispatch(downvote());
-    } else if (e.key === 'ArrowUp') {
-      this.props.dispatch(upvote());
-    }
-  }
-
-  voteDown() {
-    this.props.dispatch(downvote());
-  }
-
-  voteUp() {
-    this.props.dispatch(upvote());
-  }
 
   render() {
     return this.props.success && (
@@ -41,39 +17,7 @@ import { downvote, getArticleVoteData, upvote } from '../actions/voteActions.js'
         <h1 className="componentTitle">RESULTS</h1>
         <h2 className="trustRating">Trust Rating: {this.props.score}%</h2>
         <div className="row">
-          <div className="arrows">
-            <div className="arrowsContainer">
-              <span
-                role="button"
-                tabindex='1'
-                className={this.props.upVote ? 'arrow arrowUpSelected' : 'arrow'}
-                onClick={this.voteUp.bind(this)}
-                onKeyPress={this.handleKeyPress.bind(this)}
-              >
-                ↑
-              </span>
-              <Badge
-                badgeContent={this.props.upVoteCount}
-                primary
-              />
-            </div>
-            <div className="arrowsContainer">
-              <span
-                role="button"
-                tabindex='2'
-                className={this.props.downVote ? 'arrow arrowDownSelected' : 'arrow'}
-                onClick={this.voteDown.bind(this)}
-                onKeyPress={this.handleKeyPress.bind(this)}
-              >
-                ↓
-              </span>
-              <Badge
-                badgeContent={this.props.downVoteCount}
-                secondary
-              />
-            </div>
-          </div>
-          <div />
+          <ResultsVote />
           <div className="summaryContainer">
             <p><b>Summary: </b>
               <br />
