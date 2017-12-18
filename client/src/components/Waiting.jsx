eeab9b4b03e1c92c286cb3c34a18b52b797b832b
dwrz <dwrz@dwrz.net>
2017-12-18 18:53:15 +0000

    Add conditional display logic

--- client/src/components/Waiting.jsx
+++ client/src/components/Waiting.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import CircularProgress from 'material-ui/CircularProgress';
 
-const Waiting = () => {
-  return (
+const Waiting = props => (
+  props.display &&
     <div>
       <div className="container">
         <h1>ANALYZING...</h1>
@@ -12,7 +12,6 @@ const Waiting = () => {
         <CircularProgress />
       </div>
     </div>
-  );
-};
+);
 
 export default Waiting;
