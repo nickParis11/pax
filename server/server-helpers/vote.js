7d01cacea44a88d6dd9591eab0f7c0620457708b
Katelyn Stamas <cakelyn@gmail.com>
2017-12-18 23:19:36 -0500

    Set up getVotes

--- server/server-helpers/vote.js
+++ server/server-helpers/vote.js
@@ -4,8 +4,7 @@ const user = require('../db/controllers/userController.js');
 const submitVote = (session, article, upvote) => {
   user.get({ body: { username: session } }, (user) => {
     vote.makeVote(user.dataValues.id, article, upvote, (article) => {
-      console.log('=========> SUBMIT VOTE ARTICLE VALUE:', article[1])
-      // vote.getVotes(article);
+      vote.getVotes(article[1][0].dataValues.articleId);
     });
   });
 }
