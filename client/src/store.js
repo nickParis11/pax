7b610ad71d3ccaaca9110d8a37906d3d20f1c3f3
Niklas <norr1000@gmail.com>
2017-12-20 19:14:45 +0100

    add subscription to store

--- client/src/store.js
+++ client/src/store.js
@@ -5,4 +5,10 @@ import reducer from './reducers';
 
 const logger = createLogger();
 const middleware = applyMiddleware(thunk, logger);
-export default createStore(reducer, middleware);
+
+const store = createStore(reducer, middleware);
+
+store.subscribe(()=> {
+  console.log('stored changed ', store.getState());
+})
+export default store
