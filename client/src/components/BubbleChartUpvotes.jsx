824fd29144f3f9444be28eee43d7b4d51ba9db09
Alexander Mire <amire51117@gmail.com>
2017-12-20 13:37:43 -0500

    bubbleChart now uses dynamic data

--- client/src/components/BubbleChartUpvotes.jsx
+++ client/src/components/BubbleChartUpvotes.jsx
@@ -6,7 +6,6 @@ import * as d3 from 'd3';
 @connect((store) => {
   return {
     height: store.analyzer.height,
-    sentiment: store.analyzer.sentiment,
     tone: store.analyzer.tone,
     width: store.analyzer.width,
   };
@@ -14,28 +13,55 @@ import * as d3 from 'd3';
   drawChart() {
     var div = new ReactFauxDOM.Element('div');
     var dataset = {
-            "children": [{"Name":"Olives","Count":4319},
-                {"Name":"Tea","Count":4159},
-                {"Name":"Mashed Potatoes","Count":2583},
-                {"Name":"Boiled Potatoes","Count":2074},
-                {"Name":"Milk","Count":1894},
-                {"Name":"Chicken Salad","Count":1809},
-                {"Name":"Vanilla Ice Cream","Count":1713},
-                {"Name":"Cocoa","Count":1636},
-                {"Name":"Lettuce Salad","Count":1566},
-                {"Name":"Lobster Salad","Count":1511},
-                {"Name":"Chocolate","Count":1489},
-                {"Name":"Apple Pie","Count":1487},
-                {"Name":"Orange Juice","Count":1423},
-                {"Name":"American Cheese","Count":1372},
-                {"Name":"Green Peas","Count":1341},
-                {"Name":"Assorted Cakes","Count":1331},
-                {"Name":"French Fried Potatoes","Count":1328},
-                {"Name":"Potato Salad","Count":1306},
-                {"Name":"Baked Potatoes","Count":1293},
-                {"Name":"Roquefort","Count":1273},
-                {"Name":"Stewed Prunes","Count":1268}]
+            // "children": [{"Name":"Olives","Count":4319},
+            //     {"Name":"Tea","Count":4159},
+            //     {"Name":"Mashed Potatoes","Count":2583},
+            //     {"Name":"Boiled Potatoes","Count":2074},
+            //     {"Name":"Milk","Count":1894},
+            //     {"Name":"Chicken Salad","Count":1809},
+            //     {"Name":"Vanilla Ice Cream","Count":1713},
+            //     {"Name":"Cocoa","Count":1636},
+            //     {"Name":"Lettuce Salad","Count":1566},
+            //     {"Name":"Lobster Salad","Count":1511},
+            //     {"Name":"Chocolate","Count":1489},
+            //     {"Name":"Apple Pie","Count":1487},
+            //     {"Name":"Orange Juice","Count":1423},
+            //     {"Name":"American Cheese","Count":1372},
+            //     {"Name":"Green Peas","Count":1341},
+            //     {"Name":"Assorted Cakes","Count":1331},
+            //     {"Name":"French Fried Potatoes","Count":1328},
+            //     {"Name":"Potato Salad","Count":1306},
+            //     {"Name":"Baked Potatoes","Count":1293},
+            //     {"Name":"Roquefort","Count":1273},
+            //     {"Name":"Stewed Prunes","Count":1268}]
         };
+    const tone = this.props.tone.document_tone;
+    const emotionList = tone.tone_categories[0].tones.map(emo =>
+      { return {
+          Name : emo.tone_name,
+          Count: Math.round(emo.score * 100)
+        }
+      }
+    );
+
+    const languageToneList = tone.tone_categories[1].tones.map(lang =>
+      { return {
+          Name : lang.tone_name,
+          Count: Math.round(lang.score * 100)
+        }
+      }
+    );
+    const socialToneList = tone.tone_categories[2].tones.map(soc =>
+      { return {
+          Name : soc.tone_name,
+          Count: Math.round(soc.score * 100)
+        }
+      }
+    );
+    const allTonesList = emotionList.concat(languageToneList, socialToneList);
+    dataset.children = allTonesList;
+
+
     var color = d3.scaleOrdinal(d3.schemeCategory20);
     var diameter = this.props.height;
     var bubble = d3.pack(dataset)
