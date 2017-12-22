99ae187a79b79fb91b5b039131a49b8f88448e60
Niklas <norr1000@gmail.com>
2017-12-22 12:06:05 +0100

    add pseudo routing logic to reducers

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import analyzer from './analyzerReducer';
 import user from './userReducer';
 import vote from './voteReducer.js';
+import dashboard from './dashboardReducer.js';
 
 export default combineReducers({
   analyzer,
   user,
   vote,
+  dashboard,
 });
--- client/src/reducers/userReducer.js
+++ client/src/reducers/userReducer.js
@@ -15,16 +15,26 @@ const initialState = {
     agreeableness: 0,
     emotional_range: 0,
   },
+  loginView : false,
+  dashboardView : false,
+  inputView : true,
+  resultView : false,
 };
 
 export default function user(state = initialState, action) {
   switch (action.type) {
+    case 'SET_ANALYZE_VIEW':
+      return { ...state, loginView: false, dashboardView : false, inputView : true, resultView : false  };
     case 'LOGIN_CHECK':
       return { ...state, login: action.payload };
     case 'LOGOUT_USER':
-      return { ...state, login: false };
+      return { ...state, login: false, dashboardView : false, loginView : true, inputView : true, resultView : false  };
     case 'USER_UPVOTE_AVERAGES':
       return { ...state, upvoteAverages: action.payload };
+    case 'SET_DASHBOARD_VIEW':
+      return { ...state, dashboardView : true, inputView : false, resultView : false };
+    case 'SET_RESULT_VIEW' : 
+      return { ...state, dashboardView : false, inputView : false, resultView : true };
     default:
       return state;
   }
