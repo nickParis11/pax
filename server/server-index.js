4ffca3458a4d0e6566f540b71b02aea0a64a0e82
dwrz <dwrz@dwrz.net>
2017-12-22 01:09:11 +0000

    Refactor routing for get votes to remove bugfix

--- server/server-index.js
+++ server/server-index.js
@@ -52,19 +52,9 @@ app.post('/api/extract', (req, res) => {
 });
 
 app.get('/api/vote/:id', (req, res) => {
-  // Params is a string, undefined will be a string, not a native value.
-  if (req.params.id !== 'undefined') {
-    vote.retrieveVotes(req.params.id, req.session.user, (votes) => {
-      res.send(votes);
-    });
-  } else { // Bug fix until non-user voteCount lookup is implemented.
-    res.send({
-      downvote: false,
-      downVoteCount: 0,
-      upvote: false,
-      upVoteCount: 0,
-    });
-  }
+  vote.retrieveVotes(req.params.id, req.session.user, (votes) => {
+    res.send(votes);
+  });
 });
 
 app.post('/api/vote', (req, res) => {
@@ -76,7 +66,7 @@ app.post('/api/vote', (req, res) => {
 // Get average score of tones user upvoted
 app.get('/api/user/upvoteAverages', (req, res) => {
   // console.log('req.session', req.session);
-  if(req.session.user) {
+  if (req.session.user) {
     userDataGetter.getUpvoteAverage(req.session.user, (toneAverages) => {
       res.send(toneAverages);
     });
