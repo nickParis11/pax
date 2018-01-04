b4bae8e708f92edd0fa8ab330ae935fd6b793378
dwrz <dwrz@dwrz.net>
2018-01-04 20:23:56 +0000

    Refactor dashboardActions to align to style guide

--- client/src/actions/dashboardActions.js
+++ client/src/actions/dashboardActions.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
-export function fetchArticles() {
+export default function fetchArticles() {
   return (dispatch) => {
     console.log('!!!!!!!!!!!!!!!!!!!!!@@@@@@@@@@@@ in fetch articles dispatcher dashboard from actions');
-    //dispatch({ type: 'FETCH_ARTICLES' });
+    // dispatch({ type: 'FETCH_ARTICLES' });
     axios.get('/api/user/allArticles')
       .then((response) => {
-        console.log('@@@@@@ response from all articles => ',response.data)
+        console.log('@@@@@@ response from all articles => ', response.data);
         dispatch({ type: 'FETCH_ARTICLES_FULFILLED', payload: response.data }); // $$$$$$$$$$$
       })
       .catch((err) => {
