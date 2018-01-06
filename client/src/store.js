5e9943b54d20e17783478aeae20679624a0afa43
Niklas <norr1000@gmail.com>
2018-01-06 01:58:17 +0100

    add conditional store logging from redux depending on user local storage + remove comments

--- client/src/store.js
+++ client/src/store.js
@@ -3,12 +3,15 @@ import { createLogger } from 'redux-logger';
 import thunk from 'redux-thunk';
 import reducer from './reducers';
 
+const local = localStorage.getItem("isPaxLocalMode") === 'true';
+
 const logger = createLogger();
-const middleware = applyMiddleware(thunk, logger);
+const middleware = local ? applyMiddleware(thunk, logger) : applyMiddleware(thunk);
 
 const store = createStore(reducer, middleware);
 
 store.subscribe(() => {
-  console.log('stored has changed ', store.getState());
+  local ? console.log('stored has changed ', store.getState()) : null;
 });
+
 export default store;
