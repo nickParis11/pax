b0c4761af6a613c38bc350cdb3bf4df171850b56
Niklas <norr1000@gmail.com>
2018-01-06 02:03:49 +0100

    update default article message in the store and set it to empty string

--- client/src/reducers/dashboardReducer.js
+++ client/src/reducers/dashboardReducer.js
@@ -1,8 +1,5 @@
 const initialState = {
-  articles: `this should be replaced with real data
- it will be ultimately
- a multi dimensionnal array with :
- ARTICLEs | SCORE | .....`, // TB REMOVED
+  articles: '', 
   visible: false,
   defaultInternalView: true,
   errorInternalView: false,
